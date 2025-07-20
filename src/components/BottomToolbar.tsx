b4609904b3a713a0d9a971eaa995da45df15c94b
Phillip Gray <phill@thebold.nz>
2025-07-20 20:31:58 +1200

    chore: fix build error

--- src/components/BottomToolbar.tsx
+++ src/components/BottomToolbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDailyReset } from '../hooks/useDailyReset';
 import { useSettings } from '../contexts/SettingsContext';
 
